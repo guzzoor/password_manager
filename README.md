aa972501cdb1949e96f47226cc5b89e1e47fece5
Jonathan <gustafsson.jonathan@yahoo.se>
2020-01-08 23:57:52 +0100

    readme + fixes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # password_manager
+
+Passwords are of great importance. People are in general very bad at using good passwords,
+and this Python program are meant to be of help for that. 
+
+For the final product:
+This program should be able to generate passwords that are hard to crack but easy to 
+remember relative to how hard it is to crack. 
+
+I will myself use this together with a password manager. Use the program to generate passwords
+and let the manager keep track of them. This is the most basic feature. The program will also 
+do some calculations about the passwords, liken entropy and how hard it is to crack. 
+
+The program should not save any password, better to let a manager to do so. It should
+however track the last time the password was changed and when its time to change it again
+give a notification. 
+
--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,6 @@
 import random
 import bcrypt 
+import hashlib
 
 class PasswordCreator:
   
@@ -7,22 +8,58 @@ class PasswordCreator:
   word_list = [line.strip() for line in open('/usr/share/dict/words')]
 
   # Return an XKCD-type password
-  def createXKCD(self):
+  def createXKCD(self, word_list=word_list, num_of_words=4):
     password = ""
-    for i in range(0,4):
-      index = random.randint(0, len(self.word_list)-1)
-      word = self.word_list[index]
+    for i in range(0,num_of_words):
+      index = random.randint(0, len(word_list)-1)
+      word = word_list[index]
       word = word[0].upper() + word[1:]
       password = password + word
     return password
+
+
+  # User can now choose the length of the words to be used
+  def createXKCD_short(self, word_len=4):
+    word_list_new = list(filter(lambda k: len(k)==word_len, self.word_list))
+    return self.createXKCD(word_list_new)
 
   # Needs to be of byte type, therefor encode
   def bcrypt_pwd(self, pw):
     return bcrypt.hashpw(pw.encode('utf8'), bcrypt.gensalt())
 
+  def md5_pwd(self, pw):
+    return hashlib.md5(pw.encode())
 
+
+  def decrypt(self):
+    str = self.md5_pwd("LampFootball").hexdigest() # Just a way to represent that we have the password
+    wd = self.word_list
+    num_of_guesses = 0
+    
+    # Will run until it finds something, else for eternity
+    while True:
+      password = self.createXKCD(word_list=wd, num_of_words=2)
+      num_of_guesses = num_of_guesses + 1
+      if(str == self.md5_pwd(password).hexdigest()):
+        print(password)
+        print(num_of_guesses)  
+        return password
+ 
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------    
+
+# Just testing 
 pc = PasswordCreator()
+
+# pc.decrypt()
+# md5_pw =pc.md5_pwd("jonathan")
+#print(md5_pw.hexdigest())
+
 password = pc.createXKCD()
-print("password before encryption: " + password)
-pencr = pc.bcrypt_pwd(password)
-print("password after encryption: " + pencr.decode())
+print(password)
+
+# print("password before encryption: " + password)
+# pencr = pc.bcrypt_pwd(password)
+# print("password after encryption: " + pencr.decode())
